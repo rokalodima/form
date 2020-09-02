9722275c032b66d88da1a5054bb3bcd0d4242744
rokalodima <rokalodmitriy@gmail.com>
2020-09-02 22:42:50 +0300

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-const getPersonBtn = document.getElementById("getPersonBtn");
-getPersonBtn.addEventListener("click", function () {
-  const inputName = document.getElementById("name");
-  const inputAge = document.getElementById("age");
-  const inputdate = document.getElementById("date");
-  const selectStudy = document.getElementById("study");
-  name = inputName.value;
-  age = inputAge.value;
-  date = inputdate.value;
-  study = selectStudy.value;
-  var modal = document.getElementById("my_modal");
-  // var btn = document.getElementById("btn_modal_window");
-  var span = document.getElementsByClassName("close_modal_window")[0];
+var getPersonBtn = document.getElementById("getPersonBtn");
+const form = document.getElementById("info");
+const inputName = document.getElementById("name");
+const inputAge = document.getElementById("age");
+const inputDate = document.getElementById("date");
+const selectStudy = document.getElementById("study");
+form.addEventListener("submit", function (event) {
+  if (!validate(inputName, inputAge, inputDate, selectStudy)) {
+    inputName.classList.add("error");
+    inputAge.classList.add("error");
+    inputDate.classList.add("error");
+    selectStudy.classList.add("error");
+  } else {
+    name = inputName.value;
+    age = inputAge.value;
+    date = inputDate.value;
+    study = selectStudy.value;
+    inputName.classList.remove("error");
+    inputAge.classList.remove("error");
+    inputDate.classList.remove("error");
+    selectStudy.classList.remove("error");
+  }
 
-  getPersonBtn.onclick = function () {
-    modal.style.display = "block";
-  };
-
-  span.onclick = function () {
-    modal.style.display = "none";
-  };
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
+  console.log(name, age, date, study);
+  event.preventDefault();
 });
+
+function validate(input) {
+  return !!input.value;
+}
+var modal = document.getElementById("my_modal");
+// var btn = document.getElementById("btn_modal_window");
+var span = document.getElementsByClassName("close_modal_window")[0];
+
+getPersonBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
